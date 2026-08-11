499f94103893347465fede221393bce9d235528a
NaveenReddyFaceIntel <naveen.badduri@faceintel.ai>
2026-08-11 08:21:10 +0530

    Intentially breaking and testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
 
-    assert response.status_code == 200
+    assert response.status_code == 500
     assert response.json() == {
         "message": "Hello GitHub Actions"
     }
